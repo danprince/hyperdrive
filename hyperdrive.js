981729b592464377a98ccf46f682af0d3e74728f
danprince <danielprince@hotmail.co.uk>
2017-02-12 10:46:58 +0000

    merge hyperdrive & store code

--- hyperdrive.js
+++ hyperdrive.js
@@ -215,12 +215,57 @@
     return true;
   }
 
+  function Store(store) {
+    var subscribers = [];
+
+    for (var key in store) {
+      if (typeof store[key] === 'function') {
+        var method = store[key];
+
+        store[key] = function storeUpdate() {
+          var result = method.apply(store, arguments);
+          notify();
+          return result;
+        };
+      }
+    }
+
+    function notify() {
+      subscribers.forEach(function(func) {
+        func(store);
+      });
+    }
+
+    function subscribe(func) {
+      subscribers.push(func);
+    }
+
+    store.subscribe = subscribe;
+
+    return store;
+  }
+
+  function connect(store, component) {
+    return Component({
+      state: store,
+      mount: function() {
+        store.subscribe(this.setState);
+      },
+      render: function() {
+        var props = Object.assign({}, this.state, this.props);
+        return [component, props];
+      }
+    });
+  }
+
   var Hyperdrive = {
     Component: Component,
     createElement: createElement,
     render: render,
     parseTag: parseTag,
-    jsx: jsx
+    jsx: jsx,
+    Store: Store,
+    connect: connect
   };
 
   if (typeof module === 'object') {
